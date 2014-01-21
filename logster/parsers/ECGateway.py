5cad7cfef9a0c62b3695ed711848227eeae19341
Adam Ward <adamw@subdesigns.net>
2014-01-22 10:51:03 +1100

    Refactored ECGateway parsers into super class

--- logster/parsers/ECGateway.py
+++ logster/parsers/ECGateway.py
@@ -8,6 +8,7 @@
 
 import csv
 import datetime
+import dateutil.parser
 import simplejson as json
 import re
 import time
@@ -24,71 +25,59 @@ def total_seconds(td):
     #  NB: need real/float division
     return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10.0**6
 
-def strptime(date_string, format='%m/%d/%y %H:%M:%S'):
-    # python2.4 backport: datetime.datetime.strptime(date_string,'%m/%d/%y %H:%M:%S')
-    #  performing equiv from docs
-    return datetime.datetime(*(time.strptime(date_string, format)[0:6]))
+def strptime(date_string):
+    return dateutil.parser.parse(date_string)
 
-class AsyncQueue(LogsterParser):
-    # TODO: change this to a super class
+class ECBaseLogsterParser(LogsterParser):
+    def parse_line(self, line):
+        for data in csv.reader(StringIO(line)):
+            if data[APP] != "COMPASS_ECCONNECT":
+                continue
 
+            s_logtime = '%s %s' % (data[DATE],data[TIME])
+            dt_logtime = strptime(s_logtime)
+            msg = json.loads(data[MSG])
+
+            # start, end, duration
+            start = strptime(msg['START'])
+            end = strptime(msg['END'])
+            td=end-start
+            duration=int(total_seconds(td))
+            msg['DURATION'] = duration
+
+            return  msg
+
+        raise LogsterParsingException, "Could not parse line"
+
+class AsyncQueue(ECBaseLogsterParser):
     def __init__(self, option_string=None):
-        '''Initialize any data structures or variables needed for keeping track
-        of the tasty bits we find in the log we are parsing.'''
         self.count = {}
         self.exec_time = {}
 
     def parse_line(self, line):
-        '''This function should digest the contents of one line at a time, updating
-        object's state variables. Takes a single argument, the line to be parsed.'''
-
         try:
-            for data in csv.reader(StringIO(line)):
-                if data[APP] != "COMPASS_ECCONNECT":
-                    continue
+            msg = super(AsyncQueue, self).parse_line(line)
+            server = msg['SERVERNAME']
+            job = msg['JOBTYPE'].split('.')[-1]
 
-                s_logtime = '%s %s' % (data[DATE],data[TIME])
-                dt_logtime = strptime(s_logtime)
-                msg = json.loads(data[MSG])
-                #print msg
+            # add one to number of executions of job
+            try:
+                self.count[job] += 1
+            except KeyError:
+                self.count[job] = 1
 
-                # server, jobtype
-                server = msg['SERVERNAME']
-                job = msg['JOBTYPE'].split('.')[-1]
-
-                # start, end, duration
-                start = strptime('%s %s' % (data[DATE], msg['START']))
-                end = strptime('%s %s' % (data[DATE], msg['END']))
-                td=end-start
-                duration=int(total_seconds(td))
-
-                # add one to number of executions of job
-                try:
-                    self.count[job] += 1
-                except KeyError:
-                    self.count[job] = 1
-
-                # record execution time of job
-                try:
-                    self.exec_time[job] += duration
-                except KeyError:
-                    self.exec_time[job] = duration
+            # record execution time of job
+            try:
+                self.exec_time[job] += msg['DURATION']
+            except KeyError:
+                self.exec_time[job] = msg['DURATION']
 
         except Exception, e:
             raise LogsterParsingException, ""
 
-
     def get_state(self, duration):
-        '''Run any necessary calculations on the data collected from the logs
-        and return a list of metric objects.'''
-        self.duration = duration
-
-        result = []
-
-        # Return a list of metrics objects
         for job, count in self.count.iteritems():
-            result += MetricObject("%s.completed" % job, count, "Jobs completeted"),
+            yield MetricObject("%s.completed" % job, count, "Jobs processed")
         for job, exec_time in self.exec_time.iteritems():
-            result += MetricObject("%s.total_exec_time" % job, exec_time, "Total job execution time in seconds"),
+            yield MetricObject("%s.total_exec_time" % job, exec_time, "Total job execution time in seconds")
 
-        return result
